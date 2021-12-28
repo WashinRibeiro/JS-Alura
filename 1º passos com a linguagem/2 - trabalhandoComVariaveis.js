7f4987d7e7c3840747e178882c8a2cdc80ee68f1
Washington Ribeiro da Silva <66757021+WashinRibeiro@users.noreply.github.com>
2021-12-27 22:09:41 -0300

    Create 2 - trabalhandoComVariaveis.js

---
+++ 1º passos com a linguagem/2 - trabalhandoComVariaveis.js
@@ -0,0 +1,14 @@
+//Console.log("Trabalhando com Variáveis");
+console.log("Trabalhando com Variáveis");
+// JS é Case Sensitve (tem diferença entre maiúsculo e minúsculo)
+
+const idade = 26;
+Idade = 12; // Funciona mas não está em uma boa prática
+
+console.log("idade com i minúsculo é: " + idade);
+console.log("Idade com I maiúscula é: " + Idade);
+
+// Diferença entre colocar const ou ou está relacionado com o escopo global do código,
+// mas por padrão, é ideal colocar o declaração (palavra reservada) "const"
+
+// Temos também o "let", mas veremos ele mais para a frente !!
