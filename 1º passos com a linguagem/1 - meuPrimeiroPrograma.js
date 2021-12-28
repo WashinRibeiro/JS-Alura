34be0154acb2977377b49f14f93a8ae7fd373fd8
Washington Ribeiro da Silva <66757021+WashinRibeiro@users.noreply.github.com>
2021-12-27 21:56:26 -0300

    Update 1 - meuPrimeiroPrograma.js

--- 1º passos com a linguagem/1 - meuPrimeiroPrograma.js
+++ 1º passos com a linguagem/1 - meuPrimeiroPrograma.js
@@ -1,1 +1,12 @@
 console.log("Meu primeiro programa"); 
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade + 3); // operador de soma
+console.log(idade - 3); // operador de subtração
+console.log(idade * 3); // operador de multiplicação
+console.log(idade / 3); // operador de divisão
+
+const idadeSomada = idade + 1;
+console.log(idadeSomada);
